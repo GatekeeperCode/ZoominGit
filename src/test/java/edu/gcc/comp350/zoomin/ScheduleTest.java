ce4b4d2ac51a586641e131a2fa3fb10ddd6f0931
gacharyzoniea <zgoniea@gmail.com>
2023-03-20 16:35:46 -0400

    StubCode pt2

---
+++ src/test/java/edu/gcc/comp350/zoomin/ScheduleTest.java
@@ -0,0 +1,16 @@
+package edu.gcc.comp350.zoomin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScheduleTest {
+
+    @Test
+    void displaySchedule() {
+    }
+
+    @Test
+    void removeClassFromSchedule() {
+    }
+}
