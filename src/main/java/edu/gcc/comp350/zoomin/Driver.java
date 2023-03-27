f6a4a783e1f573ea75bde6cba363c53346d997a1
gacharyzoniea <zgoniea@gmail.com>
2023-03-27 16:25:59 -0400

    new var name course

--- src/main/java/edu/gcc/comp350/zoomin/Driver.java
+++ src/main/java/edu/gcc/comp350/zoomin/Driver.java
@@ -10,7 +10,7 @@ public class Driver {
     public static void main(String[] args)
     {
         try {
-            readInFile("C:\\Users\\BALLMD20\\IdeaProjects\\ZoominProject\\2020-2021.csv");
+            readInFile("./CSV/2020-2021.csv");
             System.out.println(courseList.get(6).credits);
         } catch(Exception e) {
             System.out.println(e);
