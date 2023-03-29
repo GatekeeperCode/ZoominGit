78b1e860b933ed81f48c14e89d85e57eb478b4e5
GatekeeperCode <mikegburiok@gmail.com>
2023-03-29 15:11:37 -0400

    Filter Loop Added

--- src/main/java/edu/gcc/comp350/zoomin/Search.java
+++ src/main/java/edu/gcc/comp350/zoomin/Search.java
@@ -93,50 +93,61 @@ public class Search {
 	 * Working in console, will update once GUI and noSQL are implemented
 	 */
 	public void setFilters() {
-		System.out.println("What filter do you want to change? TIMESLOT, PROFESSOR, CREDITHOURS, DEPARTMENT, COURSECODE");
-		Scanner scnr = new Scanner(System.in);
-
-		boolean correctInput = false;
-
-		while(!correctInput)
+		while(true)
 		{
-			String input = scnr.next();
+			System.out.println("What filter do you want to change? TIMESLOT, PROFESSOR, CREDITHOURS, DEPARTMENT, COURSECODE");
+			Scanner scnr = new Scanner(System.in);
 
-			if(input.equalsIgnoreCase("Professor"))
+			boolean correctInput = false;
+
+			while(!correctInput)
 			{
-				System.out.println("What do you want to set the filter to?");
-				filter.setProfessor(scnr.next());
-				correctInput=true;
-			}
-			else if(input.equalsIgnoreCase("TimeSlot"))
-			{
-				System.out.println("What do you want to set the filter to?");
-				filter.setTimeSlot(scnr.next());
-				correctInput=true;
-			}
-			else if(input.equalsIgnoreCase("CreditHours"))
-			{
-				System.out.println("What do you want to set the filter to?");
-				filter.setCreditHours(scnr.nextInt());
-				scnr.next();
-				correctInput=true;
-			}
-			else if(input.equalsIgnoreCase("Department"))
-			{
-				System.out.println("What do you want to set the filter to?");
-				filter.setDepartment(scnr.next());
-				correctInput=true;
-			}
-			else if(input.equalsIgnoreCase("CourseCode"))
-			{
-				System.out.println("What do you want to set the filter to?");
-				filter.setCourseCode(scnr.next());
-				correctInput=true;
-			}
-			else
-			{
-				System.out.println("The given input does not match the specifications.");
-				System.out.println("What filter do you want to change? TIMESLOT, PROFESSOR, CREDITHOURS, DEPARTMENT, COURSECODE");
+				String input = scnr.next();
+
+				if(input.equalsIgnoreCase("Professor"))
+				{
+					System.out.println("What do you want to set the filter to?");
+					filter.setProfessor(scnr.next());
+					correctInput=true;
+				}
+				else if(input.equalsIgnoreCase("TimeSlot"))
+				{
+					System.out.println("What do you want to set the filter to?");
+					filter.setTimeSlot(scnr.next());
+					correctInput=true;
+				}
+				else if(input.equalsIgnoreCase("CreditHours"))
+				{
+					System.out.println("What do you want to set the filter to?");
+					filter.setCreditHours(scnr.nextInt());
+					scnr.next();
+					correctInput=true;
+				}
+				else if(input.equalsIgnoreCase("Department"))
+				{
+					System.out.println("What do you want to set the filter to?");
+					filter.setDepartment(scnr.next());
+					correctInput=true;
+				}
+				else if(input.equalsIgnoreCase("CourseCode"))
+				{
+					System.out.println("What do you want to set the filter to?");
+					filter.setCourseCode(scnr.next());
+					correctInput=true;
+				}
+				else
+				{
+					System.out.println("The given input does not match the specifications.");
+					System.out.println("What filter do you want to change? TIMESLOT, PROFESSOR, CREDITHOURS, DEPARTMENT, COURSECODE");
+				}
+
+				System.out.println("Would you like to add another filter (Y/N)");
+				String loopContinue = scnr.next();
+
+				if(loopContinue.equals("N"))
+				{
+					return;
+				}
 			}
 		}
 	}
