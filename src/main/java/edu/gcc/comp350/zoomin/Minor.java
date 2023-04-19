292ef1e3c2ca8e63f3afd9cc3df5ae7884eb31fc
NoYuen <yuennr20@gcc.edu>
2023-04-19 12:03:04 -0400

    Minor (class) changes/additions

--- src/main/java/edu/gcc/comp350/zoomin/Minor.java
+++ src/main/java/edu/gcc/comp350/zoomin/Minor.java
@@ -1,4 +1,23 @@
 package edu.gcc.comp350.zoomin;
+import java.util.ArrayList;
 
 public class Minor {
+    private ArrayList<Course> minorReqs;
+
+
+    public Minor(ArrayList<Course> reqs){
+        this.minorReqs = reqs;
+    }
+
+    public void addMinorReq(Course toAdd){
+        this.minorReqs.add(toAdd);
+    }
+
+    public ArrayList<Course> getMinorReqs() {
+        return minorReqs;
+    }
+
+    public void setMinorReqs(ArrayList<Course> minorReqs) {
+        this.minorReqs = minorReqs;
+    }
 }
