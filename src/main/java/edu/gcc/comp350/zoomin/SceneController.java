80dcaca8e86a8e2bac28e08f76ba7a5c3fa67626
gacharyzoniea <zgoniea@gmail.com>
2023-04-27 20:24:08 -0400

    Navigation working + class search -can now move between scenes -can now navigate home by clicking the gcc logo -create schedule button navigates to new scene

--- src/main/java/edu/gcc/comp350/zoomin/SceneController.java
+++ src/main/java/edu/gcc/comp350/zoomin/SceneController.java
@@ -1,21 +1,31 @@
 package edu.gcc.comp350.zoomin;
 
+import com.calendarfx.view.DetailedWeekView;
 import javafx.application.Platform;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 import javafx.event.ActionEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SceneController{
+    private Stage stage = null;
+    private Scene scene;
+    private Parent root;
 
     @FXML
     BorderPane mainPane;
@@ -23,28 +33,44 @@ public class SceneController{
     Pane homeCenter;
     @FXML
     Pane homeBottom;
+    @FXML
+    Pane schedule;
+    @FXML
+    HBox CourseList;
 
     @FXML
-    private void handleCreateButton(ActionEvent event){
-        System.out.println("clicked create");
-        FxmlLoader loader = new FxmlLoader();
-        Pane pane = loader.getPane("CreateDefault");
-        Pane botpane = loader.getPane("ClassList");
-        mainPane.setCenter(pane);
-        mainPane.setBottom(botpane);
-        Stage stage = (Stage) mainPane.getScene().getWindow();
-        stage.sizeToScene();
+    private void handleCreateButton(ActionEvent event) throws IOException {
+//        System.out.println("clicked create");
+//        FxmlLoader loader = new FxmlLoader();
+//        Pane pane = loader.getPane("CreateDefault");
+//        Pane botpane = loader.getPane("ClassList");
+//        mainPane.setCenter(pane);
+//        mainPane.setBottom(botpane);
+//        Stage stage = (Stage) mainPane.getScene().getWindow();
+//        stage.sizeToScene();
+        root = FXMLLoader.load(getClass().getResource("CourseSearch.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    private void handleHomeButton(ActionEvent event){
-        System.out.println("clicked home");
-        FxmlLoader loader = new FxmlLoader();
-        Pane pane = loader.getPane("GUI");
-        mainPane.setCenter(homeCenter);
-        mainPane.setBottom(homeBottom);
-        Stage stage = (Stage) mainPane.getScene().getWindow();
-        stage.sizeToScene();
+    private void handleHomeButton(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void handleCalendarButton(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Calendar.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
